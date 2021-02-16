e1207dba10d6b7df183a32f6b3e573b5bcfe605e
Disems <53857795+Disems@users.noreply.github.com>
2021-02-16 06:43:48 +0200

    Add files via upload

---
+++ install_pack.py
@@ -0,0 +1,58 @@
+import os
+import colorama
+import time
+from colorama import Fore, Back, Style
+from colorama import init
+
+init()
+print(Fore. GREEN)
+an=('|','/','-','\\')
+ian = 0
+
+while True:
+    print('loading program...',an[ian],sep='',end='\r')
+    sleep(0.1)
+    ian += 1
+    if ian is len(an):ian = 0
+    
+print(Fore. CYAN)    
+print("Hello, Lera")    
+print(Fore. MAGENTA)
+print("▄───▄")   
+print("█▀█▀█")   
+print("█▄█▄█")   
+print("─███──▄▄")   
+print("─████▐█─█")   
+print("─████───█")   
+print("─▀▀▀▀▀▀▀")   
+print(Fore. GREEN)
+os.system("apt update && apt upgrade")
+time.sleep(3)
+os.sytem(y)
+os.system("apt install bash -y && apt indtall pip -y && apt install git")
+os.sytem(y)
+time.sleep(3)
+os.sytem(y)
+os.system("apt install python -y && apt install python2")
+time.sleep(3)
+os.sytem(y)
+os.system("apt install pcap")
+os.sytem(y)
+time.sleep(4)
+os.system("apt install openssh")
+os.sytem(y)
+time.sleep(4)
+os.system("apt install scapy")
+os.sytem(y)
+time.sleep(4)
+os.system("apt install wget")
+os.sytem(y)
+time.sleep(4)
+os.system("apt install curl")
+os.sytem(y)
+time.sleep(4)
+os.system("apt install php")
+os.sytem(y)
+time.sleep(4)
+print(Fore. GREEN)
+print("Усе, закончили.")
